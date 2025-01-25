03f0b1ad195ac1d24571f38f7207054d48ec0c83
Goshasn <bujhmsorokin@yandex.ru>
2025-01-25 13:47:41 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ def web():
     return render_template('WebApp.html')  
   
 if __name__ == "__main__":  
-    app.run(debug=True, host="127.0.0.1", port='80')  
+    app.run(debug=True, host="0.0.0.0", port='80')  
