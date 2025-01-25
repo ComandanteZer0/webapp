ea1b325a92741e4a92c76753943ed7f0421dd8af
Goshasn <bujhmsorokin@yandex.ru>
2025-01-25 17:27:37 +0300

    modified:   WebApp.html modified:   __pycache__/config.cpython-311.pyc deleted:    css/accesPage.css modified:   main.py new file:   static/images/NEIMBOLD.png renamed:    assets/images/pautinka.png -> static/images/pautinka.png new file:   "static/images/\320\275\320\265\320\271\320\274\320\260\321\200\320\272.png" new file:   static/styles/accesPage.css

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template  
 from config import url
-app = Flask(__name__, template_folder='.')  
-  
+app = Flask(__name__,template_folder='.')  
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route("/")  
 def web():  
+
     return render_template(url)  
   
 if __name__ == "__main__":  
