ae97152e1841d93f9ba61f8382553c248a6b4b80
Goshasn <bujhmsorokin@yandex.ru>
2025-01-25 22:58:23 +0300

    modified:   static/Scripts/dbScript.js

--- static/Scripts/dbScript.js
+++ static/Scripts/dbScript.js
@@ -6,5 +6,6 @@
 // tg.initDataUnsafe.user.last_name // "фамилия" пользователя
 // tg.initDataUnsafe.user.username // username пользователя
 // tg.initDataUnsafe.user.language_code // код языка пользователя
+alert(1)
 let tg = window.Telegram.WebApp;
 document.getElementById('idBox').innerHTML = tg.initDataUnsafe.user.id;
