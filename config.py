18b64eb85751d91b2997c7a8637c9456723a3a4d
Goshasn <comandante.ktc@gmail.com>
2025-01-26 14:42:05 +0300

    modified:   __pycache__/config.cpython-311.pyc modified:   "assets/main/\320\263\320\273\320\260\320\262\320\275\320\260\321\217.html" modified:   assets/shop/shop.html modified:   config.py modified:   static/css/shop.css

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 url = 'WebApp.html'
 sqlDns = 'amvera-goshasn-cnpg-webapp-rw'
-botToken = ''
+botToken = '8118444300:AAFwpBXmS1Rwn7sEqBSS92C86TYEchTGtAs'
 name = "bd.db"
 main_link = "https://miniapp-goshasn.amvera.io/"
